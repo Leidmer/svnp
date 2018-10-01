f602dad217c439b38a8028058cbc8521f976d966
Sethei <38982355+sethei@users.noreply.github.com>
2018-10-01 10:40:59 +0200

    prova

--- Numero_mes_petit.js
+++ Numero_mes_petit.js
@@ -5,9 +5,12 @@ function numeroMesPetit(){
     var numero3 = parseInt(prompt("Introdueix el tercer número"))
     var numero4 = parseInt(prompt("Introdueix el quart número"))
     var numero5 = parseInt(prompt("Introdueix el cinquè número"))
+    var numero6 = parseInt(prompt("Introdueix el sisè número"))
+    var numero7 = parseInt(prompt("Introdueix el setè número"))
+    var numero8 = parseInt(prompt("Introdueix el vuitè número"))
+    var numero9 = parseInt(prompt("Introdueix el novè número"))
+    var numero10 = parseInt(prompt("Introdueix el desè número"))
 
-    var resultat = Math.min(numero1,numero2,numero3,numero4,numero5)
-    document.getElementById("idPetit").innerHTML=("El número més petit és "+resultat)
+    var resultat = Math.max(numero1,numero2,numero3,numero4,numero5,numero6,numero7,numero8,numero9,numero10)
+    document.getElementById("idPetit").innerHTML=("El número més gran és "+resultat)
 }
-
-
